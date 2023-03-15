4acc6688d50505c1b134e6af4102f3b950b58a46
Philipp Guldimann <guphilip@eu-login-29.euler.ethz.ch>
2023-03-15 13:22:13 +0100

    Separate transformer code from new code

--- transformer_xl/pytorch/__init__.py
+++
@@ -1,6 +0,0 @@
-from .utils import adaptive_softmax
-from .utils import data_parallel
-from .utils import exp_utils
-from .utils import log_uniform_sampler
-from .utils import proj_adaptive_softmax
-from .utils import vocabulary
